e75aedbe0808815d008347d759b6d978f8b66b6d
AI913 <dywchiu913@hotmail.com>
2021-06-01 00:37:19 +0900

    Initial Commit

---
+++ InvestmentTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InvestmentTracker
+//
+//  Created by 招耀華 on 1/6/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
